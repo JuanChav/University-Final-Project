8c8e36cb4adce800e9072abb5920144898a99e72
Juan Andres Chaverra De Moya <juan.chaverra@globant.com>
2024-08-11 16:20:22 -0500

    Add sense to the class Student

--- src/Data/Classes/Student.java
+++ src/Data/Classes/Student.java
@@ -1,4 +1,13 @@
 package Data.Classes;
 
-public class Student {
+public class Student extends Person {
+
+    public Student(String name, int id, int age) {
+        super(name, id, age);
+    }
+
+    @Override
+    public String showData() {
+        return "ID: " + id + "\nName: " + name + "\nAge: " + age;
+    }
 }
