25cbde32697c3274d73f13c0df8f38f391a8ef77
Juan Andres Chaverra De Moya <juan.chaverra@globant.com>
2024-08-11 16:28:34 -0500

    Add first functionalities to Class

--- src/Data/Classes/Class.java
+++ src/Data/Classes/Class.java
@@ -1,4 +1,25 @@
 package Data.Classes;
 
+import Data.Interfaces.Teacher;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Class {
+
+    private String name, assignedClassroom;
+    private List<Student> students;
+    private Teacher teacher;
+
+    public Class(String name, String assignedClassroom, Teacher teacher) {
+        this.name = name;
+        this.assignedClassroom = assignedClassroom;
+        this.teacher = teacher;
+        this.students = new ArrayList<>();
+    }
+
+    private void addStudent(Student student) {
+        this.students.add(student);
+    }
+
 }
