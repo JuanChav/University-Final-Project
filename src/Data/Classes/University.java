8cd5fef4b2d3f012f469bae61fa55f8de90fff77
Juan Andres Chaverra De Moya <juan.chaverra@globant.com>
2024-08-11 16:53:23 -0500

    Add basic functionality to University

--- src/Data/Classes/University.java
+++ src/Data/Classes/University.java
@@ -1,5 +1,39 @@
 package Data.Classes;
 
+import Data.Interfaces.Teacher;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class University {
 
+    private String name;
+    private List<FullTimeTeacher> fullTeachers;
+    private List<PartTimeTeacher> partTeachers;
+    private List<Student> students;
+    private List<Class> classes;
+
+    public University(String name) {
+        this.name = name;
+        this.fullTeachers = new ArrayList<>();
+        this.partTeachers = new ArrayList<>();
+        this.students = new ArrayList<>();
+        this.classes = new ArrayList<>();
+    }
+
+    private void addFullTimeTeachers(FullTimeTeacher fullTimeTeacher) {
+        this.fullTeachers.add(fullTimeTeacher);
+    }
+
+    private void addPartTimeTeachers(PartTimeTeacher partTimeTeacher) {
+        this.partTeachers.add(partTimeTeacher);
+    }
+
+    private void addStudent(Student student) {
+        this.students.add(student);
+    }
+
+    private void addClasses(Class class_) {
+        this.classes.add(class_);
+    }
 }
